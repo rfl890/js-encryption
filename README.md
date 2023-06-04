5e6bf5c1d617dae5b5a305d5c25d1d43217d0be3
rfl890 <somebody@example.com>
2023-06-04 13:29:42 -0400

    init

---
+++ README.md
@@ -0,0 +1,112 @@
+# js-encryption
+
+## Simple encryption for JavaScript.
+
+This module provides a simple way to encrypt data. Under the hood, it uses AES-256-CTR with HMAC-SHA256 to encrypt data. The encryption/decryption process was mainly taken from [php-encryption](https://github.com/defuse/php-encryption).
+
+# Installation
+## Browser
+You can use script tags
+```html
+<script src="https://cdn.jsdelivr.net/npm/js-encryption@1.0.0/build/js-encryption.js">
+```
+```js
+js_encryption.encrypt(...);
+```
+or AMD
+```js
+require(["/path/to/js-encryption.js"], function(js_encryption) {
+    js_encryption.encrypt(...)
+});
+```
+
+## Node.js
+You can use `require()` or `import`:
+```js
+const js_encryption = require("js-encryption");
+```
+```js
+import { encryptPassword } from "js-encryption";
+```
+
+# Examples
+
+## Encrypt and decrypt some data with a password
+```js
+const textEncoder = new TextEncoder();
+const textDecoder = new TextEncoder();
+
+const dataToEncrypt = textEncoder.encode("Super secret stuff");
+
+js_encryption.encryptPassword(
+    dataToEncrypt, 
+    "ThisIsAVerySecurePassword"
+)
+    .then(encrypted => js_encryption.decryptPassword(encrypted, "ThisIsAVerySecurePassword"))
+    .then(decrypted => console.log(textDecoder.decode(decrypted)));
+
+```
+
+## Encrypt and decrypt some data with a key
+```js
+const textEncoder = new TextEncoder();
+const textDecoder = new TextEncoder();
+
+const dataToEncrypt = textEncoder.encode("Super secret stuff");
+const encryptionKey = new Uint8Array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,21,23,24,25,26,27,28,29,30,31,32]);
+
+js_encryption.encrypt(
+    dataToEncrypt, 
+    encryptionKey
+)
+    .then(encrypted => js_encryption.decryptPassword(encrypted, encryptionKey))
+    .then(decrypted => console.log(textDecoder.decode(decrypted)));
+```
+
+# API
+
+```js
+encrypt(data: Uint8Array, key: Uint8Array): Promise<Uint8Array>
+```
+Encrypts the data with the given key. Returns the encrypted ciphertext. The key must be 32 bytes long (for AES-256).
+```js
+decrypt(data: Uint8Array, key: Uint8Array): Promise<Uint8Array>
+```
+Decrypts the ciphertext from `encrypt()` (`data`) with the given key. Returns the decrypted data. The key must be 32 bytes long (for AES-256).
+
+```js
+encryptPassword(data: Uint8Array, password: String): Promise<Uint8Array>
+```
+Encrypts the data with the given password. Returns the encrypted ciphertext. The key must be 32 bytes long (for AES-256).   
+   
+   
+```js
+decryptPassword(data: Uint8Array, password: String): Promise<Uint8Array>
+```
+
+Decrypts the ciphertext from `encryptPassword()` (`data`) with the given password. Returns the decrypted data.
+
+# License
+```
+MIT License
+
+Copyright (c) 2023 rfl890
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ src/index.js
@@ -0,0 +1,345 @@
+(function () {
+    const is_browser = typeof window === "object";
+    const is_node_js = !is_browser && typeof process === "object" && process.versions && process.versions.node;
+    const is_worker = !is_browser && typeof self === "object" && self.postMessage;
+    const is_common_js = typeof module === "object" && module.exports;
+    const is_amd = typeof define === "function" && define.amd;
+
+    const root = (is_browser && window) || (is_worker && self);
+
+    const subtle = (root && root.crypto.subtle) || (is_node_js && require("crypto").subtle);
+    if (!subtle) throw new Error("It appears that your browser does not support the SubtleCrypto API. Please upgrade to a newer browser that supports it.");
+
+    const textEncoder = new TextEncoder();
+
+    // HKDF info, to derive 2 keys for AES and HMAC
+    const hkdfinfo_AESKey = textEncoder.encode("WebCrypto|AESKey");
+    const hkdfinfo_HMACKey = textEncoder.encode("WebCrypto|HMACKey");
+
+    // Argument checking
+    const checkArg = (arg) => {
+        if (typeof arg === "undefined" || arg === null) {
+            return false;
+        }
+        return true;
+    };
+
+    // Concatenate Uint8Arrays
+    function concatUint8Arrays(...arrays) {
+        const u8arrays = [...arrays];
+        const length = u8arrays.reduce((a, c) => a + c.length, 0);
+
+        const newArray = new Uint8Array(length);
+        let current = 0;
+        u8arrays.forEach((arr) => {
+            newArray.set(arr, current);
+            current += arr.length;
+        });
+
+        return newArray;
+    }
+
+    async function deriveKeyPBKDF2(password) {
+        const passwordBytes = textEncoder.encode(password);
+        const passwordHash = await crypto.subtle.digest(
+            "SHA-256",
+            passwordBytes.buffer
+        );
+
+        const hashedKey = await crypto.subtle.importKey(
+            "raw",
+            passwordHash,
+            "PBKDF2",
+            false,
+            ["deriveBits"]
+        );
+        const salt = new Uint8Array(32);
+        crypto.getRandomValues(salt);
+
+        const pbkdf2Params = {
+            name: "PBKDF2",
+            hash: "SHA-256",
+            salt: salt,
+            iterations: 600_000,
+        };
+
+        const masterKeyBytes = await crypto.subtle.deriveBits(
+            pbkdf2Params,
+            hashedKey,
+            32 * 8
+        );
+        const masterKey = await crypto.subtle.importKey(
+            "raw",
+            masterKeyBytes,
+            "HKDF",
+            false,
+            ["deriveKey"]
+        );
+        return {
+            masterKey: masterKey,
+            salt: salt,
+        };
+    }
+
+    async function deriveKeyPBKDF2WithSalt(password, salt) {
+        const passwordBytes = textEncoder.encode(password);
+        const passwordHash = await crypto.subtle.digest(
+            "SHA-256",
+            passwordBytes.buffer
+        );
+
+        const hashedKey = await crypto.subtle.importKey(
+            "raw",
+            passwordHash,
+            "PBKDF2",
+            false,
+            ["deriveBits"]
+        );
+
+        const pbkdf2Params = {
+            name: "PBKDF2",
+            hash: "SHA-256",
+            salt: salt,
+            iterations: 600_000,
+        };
+
+        const masterKeyBytes = await crypto.subtle.deriveBits(
+            pbkdf2Params,
+            hashedKey,
+            32 * 8
+        );
+        const masterKey = await crypto.subtle.importKey(
+            "raw",
+            masterKeyBytes,
+            "HKDF",
+            false,
+            ["deriveKey"]
+        );
+        return {
+            masterKey: masterKey,
+        };
+    }
+
+    async function deriveKeyRaw(keyData, salt) {
+        if (!(keyData instanceof Uint8Array))
+            throw new TypeError("keyData must be a Uint8Array");
+        if (keyData.length !== 32) throw new Error("keyData.length must be 32");
+
+        if (checkArg(salt) && !(salt instanceof Uint8Array))
+            throw new TypeError("salt must be a Uint8Array");
+        if (checkArg(salt) && salt.length !== 32)
+            throw new Error("salt.length must be 32");
+
+        const _salt = salt || crypto.getRandomValues(new Uint8Array(32));
+        return {
+            masterKey: await crypto.subtle.importKey(
+                "raw",
+                keyData,
+                "HKDF",
+                false,
+                ["deriveKey"]
+            ),
+            salt: _salt,
+        };
+    }
+
+    async function getKeysFromMasterKey(masterKey, salt) {
+        const key_aes = await crypto.subtle.deriveKey(
+            {
+                name: "HKDF",
+                hash: "SHA-256",
+                salt: salt,
+                info: hkdfinfo_AESKey,
+            },
+            masterKey,
+            { name: "AES-CTR", length: 256 },
+            false,
+            ["encrypt", "decrypt"]
+        );
+        const key_hmac = await crypto.subtle.deriveKey(
+            {
+                name: "HKDF",
+                hash: "SHA-256",
+                salt: salt,
+                info: hkdfinfo_HMACKey,
+            },
+            masterKey,
+            { name: "HMAC", hash: "SHA-256" },
+            false,
+            ["sign", "verify"]
+        );
+        return {
+            key_aes: key_aes,
+            key_hmac: key_hmac,
+        };
+    }
+
+    async function encryptPassword(data, password) {
+        const { masterKey, salt } = await deriveKeyPBKDF2(password);
+        const { key_aes, key_hmac } = await getKeysFromMasterKey(
+            masterKey,
+            salt
+        );
+
+        const iv = new Uint8Array(16);
+        crypto.getRandomValues(iv);
+
+        const ciphertext = new Uint8Array(
+            await subtle.encrypt(
+                { name: "AES-CTR", counter: iv, length: 64 },
+                key_aes,
+                data
+            )
+        );
+        const ciphertext_full_without_hmac = concatUint8Arrays(
+            ciphertext,
+            iv,
+            salt
+        );
+
+        const hmac = new Uint8Array(
+            await crypto.subtle.sign(
+                "HMAC",
+                key_hmac,
+                ciphertext_full_without_hmac
+            )
+        );
+        return concatUint8Arrays(ciphertext_full_without_hmac, hmac);
+    }
+
+    async function decryptPassword(data, password) {
+        if (data.length < 81) {
+            /* hmac, iv, salt + at least 1 encrypted byte */
+            throw new Error("Invalid ciphertext");
+        }
+
+        const ciphertext_full_without_hmac = data.slice(0, data.length - 32);
+
+        const signature = data.slice(-32);
+        const ciphertext = data.slice(0, data.length - 80);
+        const iv = data.slice(ciphertext.length, data.length - 64);
+        const salt = data.slice(ciphertext.length + 16, data.length - 32);
+
+        const { masterKey } = await deriveKeyPBKDF2WithSalt(password, salt);
+        const { key_aes, key_hmac } = await getKeysFromMasterKey(
+            masterKey,
+            salt
+        );
+
+        const verified = await crypto.subtle.verify(
+            "HMAC",
+            key_hmac,
+            signature,
+            ciphertext_full_without_hmac
+        );
+        if (verified !== true) {
+            throw new Error("Decryption error");
+        }
+
+        try {
+            return new Uint8Array(await crypto.subtle.decrypt(
+                { name: "AES-CTR", counter: iv, length: 64 },
+                key_aes,
+                ciphertext
+            ));
+        } catch (e) {
+            throw new Error("Decryption error");
+        }
+    }
+
+    async function encrypt(data, key) {
+        const { masterKey, salt } = await deriveKeyRaw(key);
+        const { key_aes, key_hmac } = await getKeysFromMasterKey(
+            masterKey,
+            salt
+        );
+        const iv = new Uint8Array(16);
+        crypto.getRandomValues(iv);
+
+        const ciphertext = new Uint8Array(
+            await subtle.encrypt(
+                { name: "AES-CTR", counter: iv, length: 64 },
+                key_aes,
+                data
+            )
+        );
+        const ciphertext_full_without_hmac = concatUint8Arrays(
+            ciphertext,
+            iv,
+            salt
+        );
+
+        const hmac = new Uint8Array(
+            await crypto.subtle.sign(
+                "HMAC",
+                key_hmac,
+                ciphertext_full_without_hmac
+            )
+        );
+        return concatUint8Arrays(ciphertext_full_without_hmac, hmac);
+    }
+
+    async function decrypt(data, key) {
+        if (data.length < 81) {
+            /* hmac, iv, salt + at least 1 encrypted byte */
+            throw new Error("Invalid ciphertext");
+        }
+
+        const ciphertext_full_without_hmac = data.slice(0, data.length - 32);
+
+        const signature = data.slice(-32);
+        const ciphertext = data.slice(0, data.length - 80);
+        const iv = data.slice(ciphertext.length, data.length - 64);
+        const salt = data.slice(ciphertext.length + 16, data.length - 32);
+
+        const { masterKey } = await deriveKeyRaw(key, salt);
+        const { key_aes, key_hmac } = await getKeysFromMasterKey(
+            masterKey,
+            salt
+        );
+
+        const verified = await crypto.subtle.verify(
+            "HMAC",
+            key_hmac,
+            signature,
+            ciphertext_full_without_hmac
+        );
+        if (verified !== true) {
+            throw new Error("Decryption error");
+        }
+
+        try {
+            return new Uint8Array(await crypto.subtle.decrypt(
+                { name: "AES-CTR", counter: iv, length: 64 },
+                key_aes,
+                ciphertext
+            ));
+        } catch (e) {
+            throw new Error("Decryption error");
+        }
+    }
+    if (is_common_js) {
+        module.exports = {
+            encrypt,
+            decrypt,
+            encryptPassword,
+            decryptPassword
+        }
+    } else if ((is_browser || is_worker) && !is_amd) {
+        root.js_encryption = {
+            encrypt,
+            decrypt,
+            encryptPassword,
+            decryptPassword
+        }
+    } else if (is_amd) {
+        define(function() {
+            return {
+                encrypt,
+                decrypt,
+                encryptPassword,
+                decryptPassword
+            }
+        });
+    }
+})();
